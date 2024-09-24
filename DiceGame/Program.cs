c4b7c4ddd7268753ca0e5bf60ed33b9de8b2e344
Tajsan <theo.alkemyr@elev.ga.lbs.se>
2024-09-24 13:36:17 +0200

    Dicegame

---
+++ DiceGame/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DiceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till spel 21");
+            Console.WriteLine("Vill du slå eller stanna?");
+            
+            string answer = Console.ReadLine();
+
+            int playerpoints = 0;
+
+            while (answer != "stanna")
+            {
+                Random rand = new Random();
+
+                int DiceNumber = rand.Next(1, 7);
+
+                playerpoints += DiceNumber;
+
+                Console.WriteLine("Du slog " + DiceNumber + ", dina poäng är nu " + playerpoints);  
+
+                answer = Console.ReadLine();
+
+
+            }
+
+            int dealerpoints = 0;
+            while (dealerpoints <= 17)
+            {
+                Random rand = new Random();
+
+                int DiceNumber = rand.Next(1, 7);
+
+                dealerpoints += DiceNumber;
+
+                Console.WriteLine("Dealern slog " + DiceNumber + ", Dealerns poäng är nu " + dealerpoints);
+            }
+
+
+
+
+
+
+        }
+    }
+}
